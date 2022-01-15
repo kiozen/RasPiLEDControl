7f7f6b89a6553630e0a0bce6b2ba0edfc5b2aa04
Oliver Eichler <oliver.eichler@gmx.de>
2022-01-15 16:28:32 +0100

    Update sunrise animation

--- script/sunrise.py
+++ script/sunrise.py
@@ -8,22 +8,26 @@ def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0)
     c2=np.array(mpl.colors.to_rgb(c2))
     return mpl.colors.to_rgb((1-mix)*c1 + mix*c2)
 
+NUM_LED = 300
 
 j = {"data" : [], "name": "Magic Sunrise", "description" : "Start your morning with a beautiful artificial sunrise while the rest of the world is still in the dark."}
 
 total_time = 0
 c1 = "#030100"
-c2 = "#783010"
-n = 50
-for x in range(n):
-    red,green,blue = colorFader(c1,c2,x/n)    
+c2 = "#3c1403"
+
+
+m = 10
+for x in range(m):
+    red,green,blue = colorFader(c1,c2,x/m)  
     color = int(round(red*255))<<16|int(round(green*255))<<8|int(round(blue*255))
-    for k in range(0, 300, 5):
-        j["data"].append([300, [color]*k])
+
+    for n in range(NUM_LED):
+        j["data"].append([300, [color]*n])
         total_time += 300
 
 
-#j["data"].append([600000, [color]*300])
+j["data"].append([600000, [color]*300])
 
 print(f"total time {total_time/60000} minutes")
 
